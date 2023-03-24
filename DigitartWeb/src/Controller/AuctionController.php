7d31865b221e692f2ccc189624f441b302689a9a
loukil33 <80423427+loukil33@users.noreply.github.com>
2023-03-24 18:18:57 +0100

    include template

--- DigitartWeb/src/Controller/AuctionController.php
+++ DigitartWeb/src/Controller/AuctionController.php
@@ -38,4 +38,18 @@ class AuctionController extends AbstractController
         }
         return $this->render('auction/add_to_auction_admin.html.twig', ['form' => $form->createView(),]);
     }
+    #[Route('/showfront', name: 'showfrontpage')]
+    public function display_front(): Response
+    {
+        return $this->render('home.html.twig', [
+            
+        ]);
+    }
+    #[Route('/showback', name: 'showbackpage')]
+    public function display_back(): Response
+    {
+        return $this->render('back.html.twig', [
+            
+        ]);
+    }
 }
