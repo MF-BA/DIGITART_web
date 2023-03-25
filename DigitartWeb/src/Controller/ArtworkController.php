5ca5c9c4a75474189734789e161517507a4b5796
MohamedLaatar <mohamed.laatar@esprit.tn>
2023-03-25 18:40:23 +0100

    crud room and artwork unfinished

--- DigitartWeb/src/Controller/ArtworkController.php
+++ DigitartWeb/src/Controller/ArtworkController.php
@@ -2,24 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\Artwork;
+use App\Form\ArtworkType;
+use App\Repository\ArtworkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/artwork')]
 class ArtworkController extends AbstractController
 {
-    #[Route('/artworkFront', name: 'app_artwork_Front')]
-    public function display_front(): Response
+    #[Route('/', name: 'app_artwork_index', methods: ['GET'])]
+    public function index(ArtworkRepository $artworkRepository): Response
     {
-        return $this->render('artwork/artworkFront.html.twig', [
-            'controller_name' => 'ArtworkController',
+        return $this->render('artwork/index.html.twig', [
+            'artworks' => $artworkRepository->findAll(),
         ]);
     }
-    #[Route('/artworkBack', name: 'app_artwork_Back')]
-    public function display_back(): Response
+
+    #[Route('/new', name: 'app_artwork_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, ArtworkRepository $artworkRepository): Response
     {
-        return $this->render('artwork/artworkBack.html.twig', [
-            'controller_name' => 'ArtworkController',
+        $artwork = new Artwork();
+        $form = $this->createForm(ArtworkType::class, $artwork);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $artworkRepository->save($artwork, true);
+
+            return $this->redirectToRoute('app_artwork_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('artwork/new.html.twig', [
+            'artwork' => $artwork,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{idArt}', name: 'app_artwork_show', methods: ['GET'])]
+    public function show(Artwork $artwork): Response
+    {
+        return $this->render('artwork/show.html.twig', [
+            'artwork' => $artwork,
+        ]);
+    }
+
+    #[Route('/{idArt}/edit', name: 'app_artwork_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Artwork $artwork, ArtworkRepository $artworkRepository): Response
+    {
+        $form = $this->createForm(ArtworkType::class, $artwork);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $artworkRepository->save($artwork, true);
+
+            return $this->redirectToRoute('app_artwork_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('artwork/edit.html.twig', [
+            'artwork' => $artwork,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{idArt}', name: 'app_artwork_delete', methods: ['POST'])]
+    public function delete(Request $request, Artwork $artwork, ArtworkRepository $artworkRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$artwork->getIdArt(), $request->request->get('_token'))) {
+            $artworkRepository->remove($artwork, true);
+        }
+
+        return $this->redirectToRoute('app_artwork_index', [], Response::HTTP_SEE_OTHER);
     }
 }
---
+++ DigitartWeb/src/Form/ArtworkType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Artwork;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArtworkType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('artworkName')
+            ->add('idArtist')
+            ->add('artistName')
+            ->add('dateArt')
+            ->add('description')
+            ->add('imageArt')
+           # ->add('idRoom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Artwork::class,
+        ]);
+    }
+}
