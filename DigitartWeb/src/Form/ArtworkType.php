d6b92abe96b7017db5658c4f8dae144af76d0006
MohamedLaatar <mohamed.laatar@esprit.tn>
2023-03-26 18:31:33 +0100

    room and artwork form updataed

--- DigitartWeb/src/Form/ArtworkType.php
+++ DigitartWeb/src/Form/ArtworkType.php
@@ -4,10 +4,16 @@ namespace App\Form;
 
 use App\Entity\Artwork;
 use App\Entity\Room;
+use App\Entity\Users;
+
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType ;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\LessThanOrEqual;
 
 class ArtworkType extends AbstractType
 {
@@ -15,9 +21,27 @@ class ArtworkType extends AbstractType
     {
         $builder
             ->add('artworkName')
-            ->add('idArtist')
-            ->add('artistName')
-            ->add('dateArt')
+            ->add('idArtist', EntityType::class, [
+                'class' => Users::class,
+                'label' => 'existing artist ',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->where('u.role = :role')
+                        ->setParameter('role', 'Artist')
+                        ->orderBy('u.lastname', 'ASC');
+                },
+                'choice_label' => 'lastname',
+                'placeholder' => 'Select an Artist', // optional placeholder text
+            ])
+            ->add('artistName', TextType::class, [
+                'attr' => ['id' => 'artistName']
+            ])
+            ->add('dateArt', DateType::class, [
+                'widget' => 'single_text',
+                'label' => 'artwork date ',
+                'attr' => ['max' => (new \DateTime())->format('Y-m-d')],
+                
+            ])
             ->add('description')
             ->add('imageArt')
             ->add('idRoom', EntityType::class, [
--- DigitartWeb/src/Form/RoomType.php
+++ DigitartWeb/src/Form/RoomType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Room;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,7 +15,15 @@ class RoomType extends AbstractType
         $builder
             ->add('nameRoom')
             ->add('area')
-            ->add('state')
+            ->add('state', ChoiceType::class, [
+                'choices' => [
+                    'Available' => true,
+                    'Unavailable' => false,
+                ],
+                'expanded' => false, // to display as dropdown list
+                'multiple' => false, // to allow selecting only one option
+                'placeholder' => 'Select a state', // optional placeholder text
+            ])
             ->add('description')
         ;
     }
