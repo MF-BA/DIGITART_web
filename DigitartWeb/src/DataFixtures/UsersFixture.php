5344de7f2dcdec0e6223a2d0d280afa29b87bb64
loukil33 <mohamedaziz.loukil@esprit.tn>
2023-04-28 11:41:56 +0100

    hashpassword-fixture

--- DigitartWeb/src/DataFixtures/UsersFixture.php
+++ DigitartWeb/src/DataFixtures/UsersFixture.php
@@ -6,20 +6,31 @@ use App\Entity\Users;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class UsersFixture extends Fixture
 {
+    private $userPasswordHasher;
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+       
+    }
     public function load(ObjectManager $manager): void
     {
+        
         $faker = Factory::create();
+           
         for($i=0; $i<50; $i++)
         {
+            
          $user = new Users();
          $user->setFirstname($faker->firstName);
          $user->setLastname($faker->lastName);
-         $user->setEmail($faker->email);
+         $user->setEmail($faker->email); 
          $user->setAddress($faker->address);
-         $user->setPassword($faker->password);
+         $user->setPassword($this->userPasswordHasher->hashPassword($user,$faker->password()));
          $user->setGender($faker->randomElement(['Male', 'Female']));
          $user->setRole($faker->randomElement(['Admin', 'Subscriber', 'Artist', 'Gallery Manager',
          'Auction Manager',
