ed717a9c66552f29f1a837210f9e7dbed1fd8d16
MohamedLaatar <mohamed.laatar@esprit.tn>
2023-05-04 00:10:20 +0100

    merge

--- DigitartWeb/src/Controller/ArtworkController.php
+++ DigitartWeb/src/Controller/ArtworkController.php
@@ -74,7 +74,10 @@ class ArtworkController extends AbstractController
 
         ]);
     }
-   
+   /*
+test merge
+
+   */
 
     #[Route('/artwork/new', name: 'app_artwork_new', methods: ['GET', 'POST'])]
     public function new(Request $request, ArtworkRepository $artworkRepository): Response
