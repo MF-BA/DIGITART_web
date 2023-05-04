69ae5a59cdd7f05f5610709f36b5a2ba8917d4eb
loukil33 <mohamedaziz.loukil@esprit.tn>
2023-05-04 15:55:25 +0100

    merge fedi+aziz

--- DigitartWeb/config/bundles.php
+++ DigitartWeb/config/bundles.php
@@ -22,8 +22,6 @@ return [
     MercurySeries\FlashyBundle\MercurySeriesFlashyBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    
 ];
